08d61d3d4bc6ca09a2057362a1052e35e24e3c40
Swapnil Bhandarkar <149192082+SwapnilBhandarkar@users.noreply.github.com>
2024-05-02 13:29:28 +0530

    Add files via upload

---
+++ 2_5/hello.js
@@ -0,0 +1,10 @@
+// const fun2=(a,b)=>{
+//     return a+b;
+// }
+// console.log("addition is "+fun2(10,50))
+
+function fun1(a,b){
+   
+    console.log("aae bhai")
+    }
+    setTimeout(fun1,100000)
